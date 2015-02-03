9ea632046472f4136f246b53797d653f79e58178
Mark Topper <mail@webman.io>
2015-02-03 15:41:02 +0100

    Create camel_case.php

---
+++ src/camel_case.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Translate a string into camel case
+ * like: camel_case => camelCase
+ *
+ * @param   string  $str  string in snake case format
+ * @return  string  $str  translated into camel case format
+ */
+function camel_case($str) {
+  $str[0] = strtolower($str[0]);
+  $func = create_function('$c', 'return "_" . strtolower($c[1]);');
+  return preg_replace_callback('/([A-Z])/', $func, $str);
+}
