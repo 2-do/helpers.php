fde0722eebb52d2a2ac7b8f2f5c8ede9e7c41e93
Mark Topper <mail@webman.io>
2015-02-18 09:43:53 +0100

    Create dd.php

---
+++ src/dd.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Dumps out all the arguments and end execution
+ * like: dd(array('foo' => 'bar'), true, false, null, 'test');
+ *
+ * @param   array   $array  the array to add the given key to
+ * @param   string  $key    the key to add into the array if not exists
+ * @param   string  $value  the value for the key in array
+ * @return  array   $array  the array in return
+ */
+function dd()
+{
+  $args = func_get_args();
+  call_user_func_array('var_dump', $args);
+  die();
+}
