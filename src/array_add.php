fb5c7dbd4961075356c646381932a34c855a0240
Mark Topper <mail@webman.io>
2016-03-06 12:37:27 -0500

    Applied fixes from StyleCI

--- src/array_add.php
+++ src/array_add.php
@@ -1,21 +1,21 @@
 <?php
 /**
  * Adds a key to the array if not allready exists
- * like: 
+ * like:
  *      $array = array('foo' => 'bar');
- *      $array = array_add($array, 'key', 'value');
+ *      $array = array_add($array, 'key', 'value');.
  *
- * @param   array   $array  the array to add the given key to
- * @param   string  $key    the key to add into the array if not exists
- * @param   string  $value  the value for the key in array
- * @return  array   $array  the array in return
+ * @param array  $array the array to add the given key to
+ * @param string $key   the key to add into the array if not exists
+ * @param string $value the value for the key in array
+ *
+ * @return array $array  the array in return
  */
 function array_add($array, $key, $value)
 {
-	if (!isset($array[$key]))
-	{
-		$array[$key] = $value;
-	}
-	
-	return $array;
+    if (!isset($array[$key])) {
+        $array[$key] = $value;
+    }
+
+    return $array;
 }
--- src/snake_case.php
+++ src/snake_case.php
@@ -1,13 +1,16 @@
 <?php
 /**
  * Translate a string into snake case
- * like: snakeCase => snake_case
+ * like: snakeCase => snake_case.
  *
- * @param   string  $str  string in camel case format
- * @return  string  $str  translated into snake case format
+ * @param string $str string in camel case format
+ *
+ * @return string $str  translated into snake case format
  */
-function snake_case($str) {
-  $str[0] = strtolower($str[0]);
-  $func = create_function('$c', 'return "_" . strtolower($c[1]);');
-  return preg_replace_callback('/([A-Z])/', $func, $str);
+function snake_case($str)
+{
+    $str[0] = strtolower($str[0]);
+    $func = create_function('$c', 'return "_" . strtolower($c[1]);');
+
+    return preg_replace_callback('/([A-Z])/', $func, $str);
 }
